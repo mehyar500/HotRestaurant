f1c506905287216fe6132fe78afb84b3329d5cda
mehyar500 <mehyar500@gmail.com>
2017-08-31 22:08:44 -0400

    final updates

--- server.js
+++ server.js
@@ -22,13 +22,21 @@ app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "index.html"))
 });
 
-app.get("/api/tables", function(req, res) {
-	res.sendFile(path.join(__dirname, "tables.html"))
+app.get("/tables", function(req, res) {
+	res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/api/reserve", function(req, res) {
-	res.sendFile(path.join(__dirname, "reserve.html"))
+app.get("/reserve", function(req, res) {
+	res.sendFile(path.join(__dirname, "reserve.html"));
 });
+
+app.get("/api/tables", function(req, res) {
+	res.json(reservations);
+});
+
+// app.get("/api/reserve", function(req, res) {
+// 	res.sendFile(path.join(__dirname, "reserve.html"))
+// });
 
 app.get("/api/new2", function(res, res) {
 	res.json(reservations);
